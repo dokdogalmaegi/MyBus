4c82fec5b92bd01118805b8b2ed4287a8f4ad041
임준영 <clik202012@gmail.com>
2021-07-18 21:40:28 +0900

    Toast Test

--- app/src/main/java/com/example/open_bus/MainActivity.java
+++ app/src/main/java/com/example/open_bus/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.open_bus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button TestBtn = findViewById(R.id.APIBtn);
+
+        TestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "테스트 메세지입니다.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
